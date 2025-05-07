d52e37c1e802c18afc6a4d6877e4a9f296e4182b
Khawaja Abdul Hafeez <abdul.hafeez@acm-finance.com>
2025-05-07 19:47:19 +0500

    added aws codepipeline scripts

--- docker-scripts-stage/start.sh
+++ docker-scripts-stage/start.sh
@@ -7,4 +7,4 @@ aws ecr get-login-password --region us-east-1 | docker login --username AWS --pa
 docker pull 533623287468.dkr.ecr.us-east-1.amazonaws.com/sample-reactapp:stage-latest
 
 # Run the Docker container 
-docker run -dit -p 3000:3000 --restart on-failure:5 --name sample-reactapp 533623287468.dkr.ecr.us-east-1.amazonaws.com/sample-reactapp:stage-latest
+docker run -dit -p 3005:3000 --restart on-failure:5 --name sample-reactapp 533623287468.dkr.ecr.us-east-1.amazonaws.com/sample-reactapp:stage-latest
